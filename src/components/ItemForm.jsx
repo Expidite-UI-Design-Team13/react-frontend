3ef6016ee83d4bc7ac0a7481b6ba0eef6d6c6ccd
Chance Onyiorah <chanceonyiorah@gmail.com>
2023-12-01 20:29:00 -0500

    separated item form into component to reuse later for edit form

---
+++ src/components/ItemForm.jsx
@@ -0,0 +1,206 @@
+import { useState, useRef, useEffect } from "react";
+import { 
+    Button,
+    TextField,
+    FormControl,
+    Box,
+    Stack,
+    MenuItem,
+    Select,
+    IconButton,
+ } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import { ButtonDropDown } from './ButtonDropDown';
+import HighlightOffRoundedIcon from '@mui/icons-material/HighlightOffRounded';
+
+export function ItemForm(props) {
+    const [categories, setCategories] = useState([]);
+    const [locations, setLocations] = useState([]);
+
+    const fileInput = useRef();
+
+    const fetchOptions = async () => { 
+        // TODO: replace user id with authentication user id 
+        // need to do user login and signup
+
+        try {
+            const categoriesRes = await fetch(`http://127.0.0.1:5000/api/categories`, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + props.token
+                },
+                body: JSON.stringify({
+                    user_id: props.id
+                })
+            })
+            const categoriesData = await categoriesRes.json()
+            const categoriesArray = []
+            
+            for (let i = 0; i < categoriesData.length; i++)
+                categoriesArray.push(categoriesData[i]["category"])
+
+            setCategories(categoriesArray);
+        } catch (error) {
+            if (error.response) {
+                console.log(error.response)
+                console.log(error.response.status)
+                console.log(error.response.headers)
+            }
+        }  
+        
+        try {
+            const locationsRes = await fetch(`http://127.0.0.1:5000/api/locations`, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + props.token
+                },
+                body: JSON.stringify({
+                    user_id: props.id
+                })
+            })
+            const locationsData = await locationsRes.json()
+            const locationsArray = []
+            
+            for (let i = 0; i < locationsData.length; i++)
+                locationsArray.push(locationsData[i]["location"])
+
+            setLocations(locationsArray);
+        } catch (error) {
+            if (error.response) {
+                console.log(error.response)
+                console.log(error.response.status)
+                console.log(error.response.headers)
+            }
+        } 
+    }
+
+    useEffect(() => {
+        fetchOptions();
+    }, [])
+
+    return (
+        <div>
+            <Box component="form" noValidate autoComplete="off" onSubmit={props.handleSubmit} sx={{ mt: 2, paddingLeft: '15px', paddingRight: '15px', paddingTop: 9, paddingBottom: 0.7, fontFamily: "'Lato', sans-serif", fontSize: '18px' }}>
+            <Box sx={{ padding: '15px', paddingTop: "0px", border: 3, borderColor: "#BEE3DB", borderRadius: 2, boxShadow: 5, backgroundColor: "#FAF9F9" }}>
+                <Stack direction="row" spacing={2} sx={{paddingTop: '10px', paddingBottom: '10px'}}>
+                    <Box sx={{fontSize: '18px', color: '#2A2E38', paddingTop: "6px"}}>Name:</Box>
+                    <TextField
+                        fullWidth
+                        variant="outlined"
+                        value={props.name}
+                        size='small'
+                        onChange={e => props.setName(e.target.value)}
+                    />
+                </Stack>
+                <Stack direction="row" spacing={2} alignItems="top" sx={{ mt: 0 }}>
+                    <Box sx={{fontSize: '18px', color: '#2A2E38'}}>Picture:</Box>
+                    {props.image === ''  ? (
+                        <IconButton onClick={() => {
+                            fileInput.current.click(); 
+                        }}>
+                            <Box sx={{backgroundColor: "#D9D9D9", width: '97px', height: '125px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                <AddIcon fontSize='large' />
+                            </Box>
+                        </IconButton>
+                    ): 
+                    (                                
+                        <Box sx={{backgroundColor: "#D9D9D9", display: 'flex', alignItems: "center"}}>
+                            <HighlightOffRoundedIcon  
+                                sx={{ color: '#555B6E', fontSize: '16px', position: 'absolute', marginLeft: 14.5, marginBottom: 15.5}}
+                                onClick={() => props.setImage('')}
+                                />
+                            <img src={require(`../components/images/${props.image}`)} width='100%' height='125px'/>
+                        </Box>
+                    )}
+                    <input
+                        type="file"
+                        hidden
+                        accept="image/*"
+                        ref={fileInput} 
+                        onChange={() => props.setImage(fileInput.current.files[0].name)}
+                    />
+                </Stack>
+                <FormControl>
+                    <Stack direction="row" spacing={1.5} alignItems="center" sx={{paddingTop: '10px', paddingBottom: '10px'}}>
+                        <Box sx={{fontSize: '18px', color: '#2A2E38'}}>Category:</Box>
+                        <Box>
+                            <ButtonDropDown
+                                items={categories}
+                                selectedItems={props.selectedCategories}
+                                setSelectedItems={props.setSelectedCategories}
+                            />
+                        </Box>
+                    </Stack>
+                </FormControl>
+                <br/>
+                <FormControl>
+                    <Stack direction="row" spacing={1.5} alignItems="center" sx={{paddingTop: '10px', paddingBottom: '10px'}}>
+                        <Box sx={{fontSize: '18px', color: '#2A2E38'}}>Location:</Box>
+                        <Box>
+                            <ButtonDropDown
+                                items={locations}
+                                selectedItems={props.selectedLocations}
+                                setSelectedItems={props.setSelectedLocations}
+                            />
+                        </Box>
+                    </Stack>
+                </FormControl>
+            </Box>
+            <Box sx={{ padding: '15px', paddingTop: "0px", marginTop: "20px", border: 3, borderColor: "#BEE3DB", borderRadius: 2, boxShadow: 5, backgroundColor: "#FAF9F9" }}>
+                <Stack direction="row" spacing={2} alignItems="center" sx={{ mt: 1 }}>
+                    <Box sx={{fontSize: '18px', color: '#2A2E38'}}>Expiration Date:</Box>
+                    <TextField
+                        type="date"
+                        value={props.expirationDate}
+                        onChange={(e) => props.setExpirationDate(e.target.value)}
+                        margin="normal"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        sx={{backgroundColor: "#BEE3DB"}}
+                        size="small"
+                    />
+                </Stack>
+                <Stack direction="row" spacing={1.6} alignItems="center" sx={{ mt: 1 }}>
+                    <Box sx={{fontSize: '18px', color: '#2A2E38'}}>Production Date:</Box>
+                    <TextField
+                        type="date"
+                        value={props.productionDate}
+                        onChange={(e) => props.setProductionDate(e.target.value)}
+                        margin="normal"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        sx={{backgroundColor: "#BEE3DB"}}
+                        size="small"
+                    />
+                </Stack>
+                <FormControl fullWidth>
+                    <Stack direction="row" spacing={1} alignItems="top" sx={{ mt: 1 }}>
+                        <Box sx={{color: '#2A2E38'}}>Alert me</Box>
+                        <Select
+                            value={props.alertDays}
+                            onChange={e => props.setAlertDays(e.target.value)}
+                            sx={{height: '25px'}}
+                        >
+                            {Array.apply(null, { length: 30 }).map((e, i) => (
+                                <MenuItem key={1+1} value={i+1}>{i+1} days</MenuItem>
+                            ))}
+                        </Select>
+                        <Box sx={{color: '#2A2E38'}}>before expiration</Box>
+                    </Stack>
+                </FormControl>
+            </Box>
+            <Box textAlign='center'>
+                <Button type="submit" fullWidth variant="contained" sx={{ height: '45px', width: '300px', mt: 11, mb: 2, backgroundColor: "#9FDDCF", color: "#2A2E38", fontSize: "20px", textTransform: 'none' }}>
+                    Submit
+                </Button>
+            </Box>
+        </Box>
+    </div>
+    )
+}
--- src/pages/NewItem.jsx
+++ src/pages/NewItem.jsx
@@ -1,20 +1,8 @@
 import { useState, useEffect, useRef } from 'react'
 import { NavBar } from "../components/NavBar";
 import { Header } from "../components/Header";
-import { 
-    Button,
-    TextField,
-    FormControl,
-    Box,
-    Stack,
-    MenuItem,
-    Select,
-    IconButton,
- } from "@mui/material";
-import AddIcon from '@mui/icons-material/Add';
-import { ButtonDropDown } from '../components/ButtonDropDown';
 import { useNavigate } from 'react-router-dom';
-import HighlightOffRoundedIcon from '@mui/icons-material/HighlightOffRounded';
+import { ItemForm } from '../components/ItemForm';
 
 export function NewItem(props) {
     const [name, setName] = useState('');
@@ -23,13 +11,8 @@ export function NewItem(props) {
     const [productionDate, setProductionDate] = useState('');
     const [alertDays, setAlertDays] = useState('');
 
-    const [categories, setCategories] = useState([]);
-    const [locations, setLocations] = useState([]);
-
     const [selectedCategories, setSelectedCategories] = useState([]);
     const [selectedLocations, setSelectedLocations] = useState([]);
-
-    const fileInput = useRef();
 
     const navigate = useNavigate();
 
@@ -73,192 +56,28 @@ export function NewItem(props) {
         }
     }
 
-    const fetchOptions = async () => { 
-        // TODO: replace user id with authentication user id 
-        // need to do user login and signup
-
-        try {
-            const categoriesRes = await fetch(`http://127.0.0.1:5000/api/categories`, {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + props.token
-                },
-                body: JSON.stringify({
-                    user_id: props.id
-                })
-            })
-            const categoriesData = await categoriesRes.json()
-            const categoriesArray = []
-            
-            for (let i = 0; i < categoriesData.length; i++)
-                categoriesArray.push(categoriesData[i]["category"])
-
-            setCategories(categoriesArray);
-        } catch (error) {
-            if (error.response) {
-                console.log(error.response)
-                console.log(error.response.status)
-                console.log(error.response.headers)
-            }
-        }  
-        
-        try {
-            const locationsRes = await fetch(`http://127.0.0.1:5000/api/locations`, {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Authorization': 'Bearer ' + props.token
-                },
-                body: JSON.stringify({
-                    user_id: props.id
-                })
-            })
-            const locationsData = await locationsRes.json()
-            const locationsArray = []
-            
-            for (let i = 0; i < locationsData.length; i++)
-                locationsArray.push(locationsData[i]["location"])
-
-            setLocations(locationsArray);
-        } catch (error) {
-            if (error.response) {
-                console.log(error.response)
-                console.log(error.response.status)
-                console.log(error.response.headers)
-            }
-        } 
-    }
-
-    useEffect(() => {
-        fetchOptions();
-    }, [])
-
     return (
         <div>
             <Header title="New Item" />
             <NavBar tab="add" />
-            <div>
-                <Box component="form" noValidate autoComplete="off" onSubmit={handleSubmit} sx={{ mt: 2, paddingLeft: '15px', paddingRight: '15px', paddingTop: 9, paddingBottom: 0.7, fontFamily: "'Lato', sans-serif", fontSize: '18px' }}>
-                    <Box sx={{ padding: '15px', paddingTop: "0px", border: 3, borderColor: "#BEE3DB", borderRadius: 2, boxShadow: 5, backgroundColor: "#FAF9F9" }}>
-                        <Stack direction="row" spacing={2} sx={{paddingTop: '10px', paddingBottom: '10px'}}>
-                            <Box sx={{fontSize: '18px', color: '#2A2E38', paddingTop: "6px"}}>Name:</Box>
-                            <TextField
-                                fullWidth
-                                variant="outlined"
-                                value={name}
-                                size='small'
-                                onChange={e => setName(e.target.value)}
-                            />
-                        </Stack>
-                        <Stack direction="row" spacing={2} alignItems="top" sx={{ mt: 0 }}>
-                            <Box sx={{fontSize: '18px', color: '#2A2E38'}}>Picture:</Box>
-                            {image === ''  ? (
-                                <IconButton onClick={() => {
-                                    fileInput.current.click(); 
-                                }}>
-                                    <Box sx={{backgroundColor: "#D9D9D9", width: '97px', height: '125px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                                        <AddIcon fontSize='large' />
-                                    </Box>
-                                </IconButton>
-                            ): 
-                            (                                
-                                <Box sx={{backgroundColor: "#D9D9D9", display: 'flex', alignItems: "center"}}>
-                                    <HighlightOffRoundedIcon  
-                                        sx={{ color: '#555B6E', fontSize: '16px', position: 'absolute', marginLeft: 14.5, marginBottom: 15.5}}
-                                        onClick={() => setImage('')}
-                                        />
-                                    <img src={require(`../components/images/${image}`)} width='100%' height='125px'/>
-                                </Box>
-                            )}
-                            <input
-                                type="file"
-                                hidden
-                                accept="image/*"
-                                ref={fileInput} 
-                                onChange={() => setImage(fileInput.current.files[0].name)}
-                            />
-                        </Stack>
-                        <FormControl>
-                            <Stack direction="row" spacing={1.5} alignItems="center" sx={{paddingTop: '10px', paddingBottom: '10px'}}>
-                                <Box sx={{fontSize: '18px', color: '#2A2E38'}}>Category:</Box>
-                                <Box>
-                                    <ButtonDropDown
-                                        items={categories}
-                                        selectedItems={selectedCategories}
-                                        setSelectedItems={setSelectedCategories}
-                                    />
-                                </Box>
-                            </Stack>
-                        </FormControl>
-                        <br/>
-                        <FormControl>
-                            <Stack direction="row" spacing={1.5} alignItems="center" sx={{paddingTop: '10px', paddingBottom: '10px'}}>
-                                <Box sx={{fontSize: '18px', color: '#2A2E38'}}>Location:</Box>
-                                <Box>
-                                    <ButtonDropDown
-                                        items={locations}
-                                        selectedItems={selectedLocations}
-                                        setSelectedItems={setSelectedLocations}
-                                    />
-                                </Box>
-                            </Stack>
-                        </FormControl>
-                    </Box>
-                    <Box sx={{ padding: '15px', paddingTop: "0px", marginTop: "20px", border: 3, borderColor: "#BEE3DB", borderRadius: 2, boxShadow: 5, backgroundColor: "#FAF9F9" }}>
-                        <Stack direction="row" spacing={2} alignItems="center" sx={{ mt: 1 }}>
-                            <Box sx={{fontSize: '18px', color: '#2A2E38'}}>Expiration Date:</Box>
-                            <TextField
-                                type="date"
-                                value={expirationDate}
-                                onChange={(e) => setExpirationDate(e.target.value)}
-                                margin="normal"
-                                InputLabelProps={{
-                                    shrink: true,
-                                }}
-                                sx={{backgroundColor: "#BEE3DB"}}
-                                size="small"
-                            />
-                        </Stack>
-                        <Stack direction="row" spacing={1.6} alignItems="center" sx={{ mt: 1 }}>
-                            <Box sx={{fontSize: '18px', color: '#2A2E38'}}>Production Date:</Box>
-                            <TextField
-                                type="date"
-                                value={productionDate}
-                                onChange={(e) => setProductionDate(e.target.value)}
-                                margin="normal"
-                                InputLabelProps={{
-                                    shrink: true,
-                                }}
-                                sx={{backgroundColor: "#BEE3DB"}}
-                                size="small"
-                            />
-                        </Stack>
-                        <FormControl fullWidth>
-                            <Stack direction="row" spacing={1} alignItems="top" sx={{ mt: 1 }}>
-                                <Box sx={{color: '#2A2E38'}}>Alert me</Box>
-                                <Select
-                                    value={alertDays}
-                                    onChange={e => setAlertDays(e.target.value)}
-                                    sx={{height: '25px'}}
-                                >
-                                    {Array.apply(null, { length: 30 }).map((e, i) => (
-                                        <MenuItem key={1+1} value={i+1}>{i+1} days</MenuItem>
-                                    ))}
-                                </Select>
-                                <Box sx={{color: '#2A2E38'}}>before expiration</Box>
-                            </Stack>
-                        </FormControl>
-                    </Box>
-                    <Box textAlign='center'>
-                        <Button type="submit" fullWidth variant="contained" sx={{ height: '45px', width: '300px', mt: 11, mb: 2, backgroundColor: "#9FDDCF", color: "#2A2E38", fontSize: "20px", textTransform: 'none' }}>
-                            Submit
-                        </Button>
-                    </Box>
-                </Box>
-            </div>
+            <ItemForm 
+                name={name}
+                setName={setName} 
+                image={image}
+                setImage={setImage} 
+                expirationDate={expirationDate}
+                setExpirationDate={setExpirationDate} 
+                productionDate={productionDate}
+                setProductionDate={setProductionDate} 
+                alertDays={alertDays}
+                setAlertDays={setAlertDays} 
+                selectedCategories={selectedCategories}
+                setSelectedCategories={setSelectedCategories} 
+                selectedLocations={selectedLocations}
+                setSelectedLocations={setSelectedLocations} 
+                handleSubmit={handleSubmit}
+                {...props}
+            />
         </div>
     );
 }
